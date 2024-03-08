a0c0884aa46e270cb638dacf15503cf901fefb7d
Minh Phước Huỳnh <huynhminhphuoc01@gmail.com>
2024-03-08 16:49:59 +0700

    update popup.js - paint toxic part correctly

--- extension/popup.js
+++ extension/popup.js
@@ -2,25 +2,33 @@
 async function scrapePage(){
   
     // bắt đầu từ đoạn này là quét nd
-    const IGNORE = ["style", "script"]
-    const walker = document.createTreeWalker(document.body, NodeFilter.SHOW_TEXT)
+    // const IGNORE = ["style", "script"]
+    // const walker = document.createTreeWalker(document.body, NodeFilter.SHOW_TEXT)
+    // const pairs = new Set()
+    // let node
+    // while ((node = walker.nextNode()) !== null) {
+    //     const parent = node.parentNode.tagName;
+      
+    //     if (IGNORE.includes(parent.toLowerCase())) {
+    //       continue;
+    //     }
+      
+    //     const value = node.nodeValue;
+      
+    //     if (value.length == 0) {
+    //       continue;
+    //     }
+    //     pairs.add(value);
+    //   }
+
     const pairs = new Set()
-    let node
-    while ((node = walker.nextNode()) !== null) {
-        const parent = node.parentNode.tagName;
-      
-        if (IGNORE.includes(parent.toLowerCase())) {
-          continue;
-        }
-      
-        const value = node.nodeValue;
-      
-        if (value.length == 0) {
-          continue;
-        }
-        pairs.add(value);
-      }
     
+    let allElement = document.body.getElementsByTagName('*');
+    for (let index = 0; index < allElement.length; index++) {
+      const element = allElement[index].innerText.trim();
+      pairs.add(element)
+    }  
+
     const arr = Array.from(pairs);
     console.log(arr);
     //tới đây
@@ -42,7 +50,7 @@ async function scrapePage(){
       .then((response) => response.json())
       .then((json)=> {
         toxic_score = json[0]
-        if(toxic_score>=0.8){
+        if(toxic_score>=0.7){
           cases.push(content); 
           //lưu mảng lại vào máy
           chrome.storage.local.set({"content":cases});
@@ -58,18 +66,34 @@ async function scrapePage(){
 }
 
 function blur_text(){
+  
   var span = document.getElementsByTagName('span');
   for (let index = 0; index < span.length; index++) {
     var element = span[index];
     element.parentNode.removeChild(element)
   }
+  
+  // var breaks = document.getElementsByTagName('br');
+  // for (let index = 0; index < breaks.length; index++) {
+  //   var element = breaks[index];
+  //   element.parentNode.removeChild(element)
+  // }
+
   console.log('blur')
   var allElement = document.body.getElementsByTagName('*');
   for (let index = 0; index < allElement.length; index++) {
     const element = allElement[index];
-    console.log(element.innerHTML)
+    
+    // if(element.tagName.toLowerCase()==='span'){
+    //   element.parentNode.removeChild(element)
+    // }
+    // if(element.tagName.toLowerCase()==='br'){
+    //   console.log(element);
+    //   element.parentNode.removeChild(element)
+    // }
     chrome.storage.local.get("content", (result)=>{
-      if(result.content.includes(element.innerHTML)){
+      console.log(result.content)
+      if(result.content.includes(element.innerText.trim())){
         console.log(element)
         element.style.color = "red"
       }
@@ -112,9 +136,9 @@ scrape.addEventListener("click", async()=>{
         chrome.storage.local.get("content", function(result){
           //alert(JSON.stringify(result.content));
           text.innerHTML = ""
-          run.innerHTML = `<b>&#127795;Detected ${result.content.length} case(s)!&#127795;	</b>`
           for (const line of result.content){
             
+            run.innerHTML = `<b>&#127795;Detected ${result.content.length} case(s)!&#127795;	</b>`
             text.innerHTML = text.innerHTML+ `<p>${JSON.stringify(line.trim()).replace(/(\\+n)/g, '')}</p>` + '\n\n';
           }
         });
@@ -126,7 +150,7 @@ scrape.addEventListener("click", async()=>{
     
     
     //Excecute script chạy cái func trên cùng
-    chrome.scripting.executeScript({
+    await chrome.scripting.executeScript({
       target: {tabId: tab.id},
       func: scrapePage
     })
